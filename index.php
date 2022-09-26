e3d38badd9f1c14897c574f7ba75440da6383ac5
Ewan Quioc <quioc.ewan@gmail.com>
2022-09-26 17:05:09 +0200

    modif pour l'affichage du projet

--- index.php
+++ index.php
@@ -14,7 +14,8 @@
     $customer1->setId(1);
 
     $projet1 = new Projet("premier projet", "patate", "racine", "sbhdof", 1, "attention", $host1, $customer1);
-    $projet1->getHost()->getNameHost();
+    var_dump($projet1->getHost()->getId());
+    var_dump($projet1->getCustomer()->getId());
 
     $environnement = new Environnement("environment1", "http://", "000.000.000",22, "ssh", "localhost", 232, $projet1);
     var_dump($environnement->getProjet()->getHost());
