dd654a657386407b569014362d6669f20fbed5b0
Ewan Quioc <quioc.ewan@gmail.com>
2022-09-26 16:02:28 +0200

    ajout de la classe Projet avec ses getter et setter

---
+++ class/Projet.php
@@ -0,0 +1,67 @@
+<?php
+    class Projet
+    {
+        private int $id;
+        private string $name;
+        private string $code;
+        private string $lastpass_folder;
+        private string $link_mock_ups;
+        private int $manage_server;
+        private string $notes;
+        private int $host_id;
+        private int $customer_id;
+
+        public function getId(): ?int
+        {
+            return $this->id;
+        }
+        public function getName(): ?string
+        {
+            return $this->name;
+        }
+        public function getCode(): ?string
+        {
+            return $this->code;
+        }
+        public function getLastpassFolder(): ?string
+        {
+            return $this->lastpass_folder;
+        }
+        public function getLinkMockUps(): ?string
+        {
+            return $this->link_mock_ups;
+        }
+        public function getManageServer(): ?int
+        {
+            return $this->manage_server;
+        }
+        public function getNotes(): ?string
+        {
+            return $this->notes;
+        }
+
+        public function setName(string $name): void
+        {
+            $this->name = $name;
+        }
+        public function setCode(string $code): void
+        {
+            $this->code = $code;
+        }
+        public function setLastPassFolder(string $lastpass_folder): void
+        {
+            $this->lastpass_folder = $lastpass_folder;
+        }
+        public function setLinkMockUps(string $link_mock_ups): void
+        {
+            $this->link_mock_ups = $link_mock_ups;
+        }
+        public function setManageServer(string $manage_server): void
+        {
+            $this->manage_server = $manage_server;
+        }
+        public function setNotes(string $notes): void
+        {
+            $this->notes = $notes;
+        }
+    }
