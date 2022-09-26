216cfad1c7e34c53d11e7c51836c2e482b49d2c8
Ewan Quioc <quioc.ewan@gmail.com>
2022-09-26 17:01:09 +0200

    indentation code Host et validation des relations avec les class associées a projet

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
     require __DIR__."./class/Host.php";
     require __DIR__."./class/Projet.php";
-<<<<<<< HEAD
-=======
     require __DIR__."./class/Environnement.php";
->>>>>>> 829823c91cdda08026eb7a7a86188abd32952255
     require __DIR__."./class/Customer.php";
 
     $host1 = new Host();
@@ -17,15 +14,9 @@
     $customer1->setId(1);
 
     $projet1 = new Projet("premier projet", "patate", "racine", "sbhdof", 1, "attention", $host1, $customer1);
-<<<<<<< HEAD
-    var_dump($projet1->getCustomer()->getId());
-    var_dump($projet1->getHost()->getId());
-    
-=======
     $projet1->getHost()->getNameHost();
 
     $environnement = new Environnement("environment1", "http://", "000.000.000",22, "ssh", "localhost", 232, $projet1);
     var_dump($environnement->getProjet()->getHost());
     var_dump($environnement->getProjet()->getCustomer());
 
->>>>>>> 829823c91cdda08026eb7a7a86188abd32952255
