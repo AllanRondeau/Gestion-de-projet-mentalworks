b47414e29f7e43e435584afddecd63b01b42a51b
Ewan Quioc <quioc.ewan@gmail.com>
2022-10-09 15:45:03 +0200

    modificationn du nom du fichier sql et indentation index.php

--- index.php
+++ index.php
@@ -15,29 +15,30 @@ $contact1 = new Contact("email@gmail.com", "06 36 98 65 74", "admin", $host1, $c
 
 <!doctype html>
 <html lang="fr">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gestion de projet</title>
-    <link href="style.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-          integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
-          crossorigin="anonymous" referrerpolicy="no-referrer">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <header>
-      <img src="images/logo-mentalworks-blanc.png" alt="logo mentalworks">
-    </header>
-    <nav>
-      <a><i class="fa-solid fa-house"></i>tableau de bord</a>
-      <a><i class="fa-regular fa-user"></i>projets</a>
-      <a><i class="fa-regular fa-building"></i>clients</a>
-      <a><i class="fa-regular fa-square-check"></i>hébergeurs</a>
-    </nav>
-    <footer></footer>
-  </body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Gestion de projet</title>
+        <link href="style.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+              integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+              crossorigin="anonymous" referrerpolicy="no-referrer">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <img src="images/logo-mentalworks-blanc.png" alt="logo mentalworks">
+        </header>
+        <nav>
+            <a><i class="fa-solid fa-house"></i>tableau de bord</a>
+            <a><i class="fa-regular fa-user"></i>projets</a>
+            <a><i class="fa-regular fa-building"></i>clients</a>
+            <a><i class="fa-regular fa-square-check"></i>hébergeurs</a>
+        </nav>
+        
+        <footer></footer>
+    </body>
 </html>
 
