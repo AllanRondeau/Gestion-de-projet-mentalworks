9761c20704a81ed51982da565dd4c44e6b1e6648
Allan Rondeau <sdrondeauallan@gmail.com>
2022-10-30 23:05:58 +0100

    Création d'un fonction pour modeler un json contenant tous les clients

--- selectCustomer.php
+++ selectCustomer.php
@@ -1,14 +1,20 @@
 <?php
 use App\HydrateCustomer;
+$listCustomer = array();
 
 function test(array $test)
 {
-    $listCustomer = array();
+
     foreach ($test as $createCustomer) 
     {
         HydrateCustomer::createCustomer(array("code" => $createCustomer["code"], "name" => $createCustomer["name"], "note" => $createCustomer["note"]));
         array_push($listCustomer, HydrateCustomer::getCustomer());
     }
-
     echo json_encode($listCustomer);
+}
+
+if ($listCustomer){
+    foreach ($listCustomer as $key => $customer){
+        echo $key;
+    }
 }
